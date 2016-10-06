cca65ea16cc9e8f738b0a55b47175025a48341a1
Alan Lira <alan.lira@insidesistemas.com.br>
2016-10-06 13:17:55 -0300

    adicionado DebugTransport

---
+++ Mailer.NET/Mailer/Transport/DebugTransport.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mailer.NET.Mailer.Response;
+
+namespace Mailer.NET.Mailer.Transport
+{
+    public class DebugTransport : AbstractTransport
+    {
+        public bool Success { get; set; }
+
+        public DebugTransport(bool success)
+        {
+            Success = success;
+        }
+
+        public override Response.EmailResponse SendEmail(Email email)
+        {
+            var response = new EmailResponse()
+            {
+                Success = Success,
+                Message = (Success ? "Email successfully sent" : "Undefined Error")
+            };
+            return response;
+        }
+    }
+}
