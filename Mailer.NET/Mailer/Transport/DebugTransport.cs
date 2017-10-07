79c0cc259f4a2185c0148f7ccf0ad468768b4176
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2017-10-07 14:29:31 -0500

    Add SendEmailAsync method to AbstractTransport. Stub out SendEmailAsync methods in all transport classes.
    Add SendEmailAsync implementation for DebugTransport
    
    Add SendEmailAsync implementation for MailgunTransport
    
    Add SendEmailAsync implementation for SmtpTransport
    
    Fix Debug configuration
    
    Remove .NET 4.0 RestSharp from project file
    
    Debug and Release configurations building.  Removed deprecated build configurations from build scripts.

--- Mailer.NET/Mailer/Transport/DebugTransport.cs
+++ Mailer.NET/Mailer/Transport/DebugTransport.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Mailer.NET.Mailer.Response;
 
 namespace Mailer.NET.Mailer.Transport
@@ -15,14 +16,25 @@ namespace Mailer.NET.Mailer.Transport
             Success = success;
         }
 
-        public override Response.EmailResponse SendEmail(Email email)
+        public override EmailResponse SendEmail(Email email)
         {
-            var response = new EmailResponse()
+            var response = GenerateDebugEmailResponse();
+            return response;
+        }
+
+        public override async Task<EmailResponse> SendEmailAsync(Email email)
+        {
+            var response = await Task.FromResult(GenerateDebugEmailResponse());
+            return response;
+        }
+
+        private EmailResponse GenerateDebugEmailResponse()
+        {
+            return new EmailResponse
             {
                 Success = Success,
                 Message = (Success ? "Email successfully sent" : "Undefined Error")
             };
-            return response;
         }
     }
 }
