76b33d9999e86779f97e7056f0ebff08543ddc4a
tetiana.zhydeikina.kn.2016@lpnu.ua <tetiana.zhydeikina.kn.2016@lpnu.ua>
2017-10-26 20:38:26 +0300

    structure of future tests

---
+++ Mailer.NET.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mailer.NET.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mailer.NET.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("156353c5-0a44-438f-8754-cea082283f2c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Mailer.NET.Tests/Rendering/RenderEmailTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mailer.NET.Tests.Rendering
+{
+
+    [TestClass]
+    public class RenderEmailTest
+    {
+        [TestMethod]
+        public void RenderEmailTestTemplateVarsIsNotNull()
+        {
+            
+        }
+
+        public RenderEmailTest()
+        {
+
+        }
+
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+    }
+}
